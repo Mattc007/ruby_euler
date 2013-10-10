b9c33eabd9301a6026e9c1d01fa2f44d237dd1ba
Matt Catrett <mcatrett@turbosquid.com>
2013-10-10 13:43:51 -0500

    add a Farey sequence generator for fun

---
+++ farey.rb
@@ -0,0 +1,16 @@
+# Displays a Farey sequence given the order
+
+def farey(order)
+  a, b, c, d = 0, 1, 1, order
+  output =  "#{a}/#{b}"
+  while c <= order
+    k = (order + b) / d
+    a, b, c, d = c, d, k*c - a, k*d - b
+    output <<  " #{a}/#{b}"
+  end
+  puts output
+end
+
+(1..8).each do |i|
+  farey i
+end
