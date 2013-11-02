94adea430ca38899cfb3fe05c91fb87292d57892
Matt Catrett <mcatrett@turbosquid.com>
2013-11-02 14:08:18 -0500

    finally complete problem 93

--- 093.rb
+++ 093.rb
@@ -49,13 +49,7 @@ end
 def apply(operands, operators)
   solutions = []
   solutions << send(operators[2], send(operators[1], send(operators[0], operands[0], operands[1]), operands[2]), operands[3])
-  solutions << send(operators[2], (send(operators[0], operands[0], send(operators[1], operands[1], operands[2]))), operands[3])
-  solutions << send(operators[0], operands[0], (send(operators[1], operands[1], (send(operators[2], operands[2], operands[3])))))
-  solutions << send(operators[0], operands[0], send(operators[2], send(operators[1], operands[1], operands[2]), operands[3]))
   solutions << send(operators[1], send(operators[0], operands[0], operands[1]), (send(operators[2], operands[2], operands[3])))
-
-  # puts "#{operands} : #{operators} => #{solutions}"
-  solutions
 end
 
 def consecutive_integers_max(solutions)
@@ -70,7 +64,7 @@ end
 t1 = Time.now
 
 digits = (0..9).map(&:to_f).to_a
-digits_sets = digits.permutation(4).to_a
+digits_sets = digits.combination(4).to_a
 
 operations = [:add, :subtract, :multiply, :divide]
 operations_sets = operations.repeated_permutation(3).to_a
@@ -78,25 +72,22 @@ operations_sets = operations.repeated_permutation(3).to_a
 max_solution = 0
 max_operands = []
 
-digits_sets.each do |operands|
-# [[1,2,3,4],[1,2,3,6],[1,2,5,8]].each do |operands|
-  next unless operands[0] < operands[1] && operands[1] < operands[2] && operands[2] < operands[3]
-  solutions_sets = operations_sets.map { |ops| apply(operands, ops) }.flatten.compact
+digits_sets.each do |operands_group|
+  solutions_sets = []
+  operands_group.permutation(4).to_a.each do |operands|
+    solutions_sets << operations_sets.map { |ops| apply(operands, ops) }.flatten.compact
+  end
+  
+  solutions_sets.flatten!.compact!
+  
   solutions = solutions_sets.uniq.sort.select { |s| s.floor == s && s > 0}.map(&:to_i).uniq
-  
   s = consecutive_integers_max(solutions)
-  
+    
   if s > max_solution
     max_solution = s
-    max_operands = operands
+    max_operands = operands_group
   end
-
-  # if (operands == [1,2,5,8] || operands == [0,1,2,4])
-  #   p operands
-  #   p solutions
-  #   p s
-  # end
-
 end
 
 puts "found #{max_operands.map(&:to_i).join('')} with max solution #{max_solution} in #{Time.now - t1}s"
+# found 1258 with max solution 51 in 1.201953s
